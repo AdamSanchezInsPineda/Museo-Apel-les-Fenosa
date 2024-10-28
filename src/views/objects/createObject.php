08b08ed7a7265dd9a09a21bea23f09e141cd7b15
davidgarciafp <david.garcia.fernandez@inslapineda.cat>
2024-10-28 17:20:21 +0100

    create v2

--- src/views/objects/createObject.php
+++ src/views/objects/createObject.php
@@ -7,14 +7,18 @@
         <h1>Afegir un registre</h1>
     </div>
     <form action="post">
-        <h2>Informació Bàsica</h2>
         <table>
+            <tr>
+                <td colspan = "2">
+                    <h2>Informació Bàsica</h2>
+                </td>     
+            </tr>
             <tr>
                 <td>
                     <label for="RegistroNº">Nº de Registre:</label>
                     <input type="text" id="RegistroNº" name="RegistroNº" required>
                 </td>
-                <td rowspan = 4>
+                <td rowspan="3">
                     <input type="file" name="imagen" src="resources/images/lupa.png" alt="Submit" height="100px">
                     <!--
                         if (is_uploaded_file ($_FILES['imagen']['tmp_name']))
@@ -38,7 +42,7 @@
                 </td>
             </tr>
             <tr>
-                <td>
+                <td >
                     <label for="Museos">Museu:</label>
                     <select name="Museos" id="Museos" required>
                         <!-- Select de Nombre de Museos -->
@@ -54,24 +58,26 @@
                     </select>
                     <!-- <input type="text" id="Autor" name="Autor" required> -->
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="Títol">Títol:</label>
                     <input type="text" id="Títol" name="Títol" required>
                 </td>
             </tr>
-            <h2>Ubicacions</h2>
             <tr>
-                <!-- Ubicacio -->
+                <td colspan = "2">
+                    <h2>Ubicacions</h2>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <!-- Ubicacio -->
+                </td>
             </tr>
             <tr>
                 <td>
                     <label for="FechaInicioUbicacion">Data Inici Ubicació:</label>
                     <input type="datetime-local" id="FechaInicioUbicacion" name="FechaInicioUbicacion" required>
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="FechaFinUbicacion">Data Final Ubicació:</label>
                     <input type="datetime-local" id="FechaFinUbicacion" name="FechaFinUbicacion">
@@ -83,11 +89,19 @@
                     <input type="text" id="ComentarioUbicacion" name="ComentarioUbicacion">
                 </td>
             </tr>
-            <h2>Propietats</h2>
+            <tr>
+                <td colspan = "2">
+                    <h2>Propietats</h2>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <label for="Altura">Altura:</label>
                     <input type="text" id="Altura" name="Altura" required>
+                </td>
+                <td>
+                    <label for="descripcion">Datació:</label>
+                    <input type="text" id="descripcion" name="descripcion" required>
                 </td>
             </tr>
             <tr>
@@ -95,11 +109,19 @@
                     <label for="Anchura">Amplada:</label>
                     <input type="text" id="Anchura" name="Anchura" required>
                 </td>
+                <td>
+                    <label for="any_inicial">Any Inicial:</label>
+                    <input type="text" id="any_inicial" name="any_inicial">
+                </td>
             </tr>
             <tr>
                 <td>
                     <label for="Profundidad">Profunditat:</label>
                     <input type="text" id="Profundidad" name="Profundidad" required>
+                </td>
+                <td>
+                    <label for="any_final">Any Final:</label>
+                    <input type="text" id="any_final" name="any_final">
                 </td>
             </tr>
             <tr>
@@ -109,26 +131,6 @@
                         <!-- Select de valor de Material -->
                     </select>
                 </td>
-            </tr></tr>
-            <tr>
-                <td>
-                    <label for="descripcion">Datació:</label>
-                    <input type="text" id="descripcion" name="descripcion" required>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="any_inicial">Any Inicial:</label>
-                    <input type="text" id="any_inicial" name="any_inicial">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="any_final">Any Final:</label>
-                    <input type="text" id="any_final" name="any_final">
-                </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="Tecnica">Tècnica:</label>
                     <select name="Tecnica" id="Tecnica">
@@ -136,7 +138,11 @@
                     </select>
                 </td>
             </tr>
-            <h2>Baixa</h2>
+            <tr>
+                <td colspan = "2">
+                    <h2>Baixa</h2>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <label for="Baja">Baixa:</label>
@@ -145,8 +151,6 @@
                         <option value="Si">Si</option>
                     </select>
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="CausaBaja">Causa Baixa:</label>
                     <select name="CausaBaja" id="CausaBaja">
@@ -159,24 +163,24 @@
                     <label for="FechaBaja">Data Baixa:</label>
                     <input type="datetime-local" id="FechaBaja" name="FechaBaja">
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="PersonaAutorizadaBaja">Persona Autorizada Baja:</label>
                     <input type="text" id="PersonaAutorizadaBaja" name="PersonaAutorizadaBaja">
                 </td>
             </tr>
-            <h2>Restauració</h2>
+            <tr>
+                <td colspan = "2">
+                    <h2>Restauració</h2>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <label for="FechaInicio">Data Inici Restauració:</label>
                     <input type="datetime-local" id="FechaInicio" name="FechaInicio">
                 </td>
-            </tr>
-            <tr>
                 <td>
-                <label for="FechaFin">Data Final Restauració:</label>
-                <input type="datetime-local" id="FechaFin" name="FechaFin">
+                    <label for="FechaFin">Data Final Restauració:</label>
+                    <input type="datetime-local" id="FechaFin" name="FechaFin">
                 </td>
             </tr>
             <tr>
@@ -184,8 +188,6 @@
                     <label for="CodigoRestauracion">Codi Restauració:</label>
                     <input type="text" id="CodigoRestauracion" name="CodigoRestauracion">
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="NombreRestaurador">Nom Restaurador:</label>
                     <input type="text" id="NombreRestaurador" name="NombreRestaurador">
@@ -197,7 +199,11 @@
                     <input type="text" id="ComentarioRestauracion" name="ComentarioRestauracion">
                 </td>
             </tr>
-            <h2>Ingrés</h2>
+            <tr>
+                <td colspan = "2">
+                    <h2>Ingrés</h2>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <label for="FormaIngreso">Forma d'Ingrés:</label>
@@ -205,8 +211,6 @@
                         <!-- Select de valor de FormaIngreso -->
                     </select>                
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="FuenteIngreso">Font d'Ingrés:</label>
                     <input type="text" id="FuenteIngreso" name="FuenteIngreso">
@@ -217,40 +221,40 @@
                     <label for="FechaIngreso">Data d'Ingrés:</label>
                     <input type="text" id="FechaIngreso" name="FechaIngreso">
                 </td>
-            </tr>
-            <tr>
                 <td>
-                    <label for="EstadoConservacion">Comentari Restauració:</label>
+                    <label for="EstadoConservacion">Estat de conservació:</label>
                     <select name="EstadoConservacion" id="EstadoConservacion">
                         <!-- Select de valor de EstadoConservacion -->
                     </select>                  
                 </td>
             </tr>
-            <h2>Exposicions</h2>
+            <tr>
+                <td colspan = "2">
+                    <h2>Exposicions</h2>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <label for="NombreExposicion">Nom:</label>
                     <input type="text" id="NombreExposicion" name="NombreExposicion">
                 </td>
+                <td>
+                    <label for="LugarExposicion">Lloc:</label>
+                    <input type="text" id="LugarExposicion" name="LugarExposicion">
+                </td>
+                     
             </tr>
             <tr>
                 <td>
                     <label for="InicioExposicion">Data Inicial:</label>
                     <input type="datetime-local" id="InicioExposicion" name="InicioExposicion">
-                </td>               
-            </tr>
-            <tr>
+                </td>
                 <td>
                     <label for="FinalExposicion">Data Final:</label>
                     <input type="datetime-local" id="FinalExposicion" name="FinalExposicion">
                 </td>
             </tr>
-            <tr>
-                <td>
-                    <label for="LugarExposicion">Lloc:</label>
-                    <input type="text" id="LugarExposicion" name="LugarExposicion">
-                </td>
-            </tr>
+                
             <tr>
                 <td>
                     <label for="TiposExposicion">Tipus:</label>
@@ -259,14 +263,16 @@
                     </select>                  
                 </td>
             </tr>
-            <h2>Altres Dades</h2>
+            <tr>
+                <td colspan = "2">
+                    <h2>Altres Dades</h2>
+                </td>
+            </tr>
             <tr>
                 <td>
                     <label for="Col·lecció">Col·lecció de Procedencia:</label>
                     <input type="text" id="Col·lecció" name="Col·lecció">
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="LugarEjecucion">Lloc d'Execució:</label>
                     <input type="text" id="LugarEjecucion" name="LugarEjecucion">
@@ -276,9 +282,7 @@
                 <td>
                     <label for="NumeroTiraje">Nº de Tiratge:</label>
                     <input type="text" id="NumeroTiraje" name="NumeroTiraje">
-                </td>               
-            </tr>
-            <tr>
+                </td>
                 <td>
                     <label for="ValoracionEconomica">Valoració Económica:</label>
                     <input type="text" id="ValoracionEconomica" name="ValoracionEconomica">
@@ -289,8 +293,6 @@
                     <label for="NumeroEjemplares">Nº d'Exemplars:</label>
                     <input type="text" id="NumeroEjemplares" name="NumeroEjemplares">
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="LugarProcedencia">Lloc de Procedencia:</label>
                     <input type="text" id="LugarProcedencia" name="LugarProcedencia">
@@ -301,8 +303,6 @@
                     <label for="OtrosNrosIdentificacion">Altres Nº Identificació:</label>
                     <input type="text" id="OtrosNrosIdentificacion" name="OtrosNrosIdentificacion">
                 </td>
-            </tr>
-            <tr>
                 <td>
                     <label for="Classificacion">Classificació Genérica:</label>
                     <select name="Classificacion" id="Classificacion">
@@ -328,8 +328,12 @@
                     <input type="text" id="HistoriaObjeto" name="HistoriaObjeto">
                 </td>
             </tr>
+            <tr>
+                <td>                
+                    <input type="submit" value="Guardar" class="submit">
+                </td>
+            </tr>
         </table>
-        <input type="submit" value="Guardar" class="submit">
     </form>
     <?php
     include "resources/components/footer.php";
