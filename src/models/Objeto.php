4ad899e788fac580661a2cb448ba0ad3a6afe6b7
davidgarciafp <david.garcia.fernandez@inslapineda.cat>
2024-11-05 09:22:14 +0100

    mas fitxas

--- src/models/Objeto.php
+++ src/models/Objeto.php
@@ -53,47 +53,49 @@
 
         function fitxesCreate(){
             $sql = "INSERT INTO Objetos (RegistroNº, UsuarioID, Imagen, Nombre, ClasificacionGenericaID, ColeccionProcedencia, Altura, Anchura, Profundidad, MaterialID, TecnicaID, AutorID, Titulo, DatacionID, UbicacionActualID, FechaRegistro, NumeroEjemplares, FormaIngresoID, FechaIngreso, FuenteIngreso, BajaID, CausaBajaID, FechaBaja, PersonaAutorizadaBaja, EstadoConservacionID, LugarEjecucion, LugarProcedencia, NumeroTiraje, OtrosNrosIdentificacion, ValoracionEconomica, Bibliografia, Descripcion, HistoriaObjeto, MuseoID, Activo) 
-            VALUES (:registroN, :usuarioID, :imagen, :nombre, :clasificacionGenericaID, :coleccionProcedencia, :altura, :anchura, :profundidad, :materialID, :tecnicaID, :autorID, :titulo, :datacionID, :ubicacionActualID, :fechaRegistro, :numeroEjemplares, :formaIngresoID, :fechaIngreso, :fuenteIngreso, :bajaID, :causaBajaID, :fechaBaja, :personaAutorizadaBaja, :estadoConservacionID, :lugarEjecucion, :lugarProcedencia, :numeroTiraje, :otrosNrosIdentificacion, :valoracionEconomica, :bibliografia, :descripcion, :historiaObjeto, :museoID, :activo)";
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+            // Preparar la declaración
             $stmt = $db->prepare($sql);
 
-            $stmt->bindParam(':registroN', $registroN);
-            $stmt->bindParam(':usuarioID', $usuarioID);
-            $stmt->bindParam(':imagen', $imagen);
-            $stmt->bindParam(':nombre', $nombre);
-            $stmt->bindParam(':clasificacionGenericaID', $clasificacionGenericaID);
-            $stmt->bindParam(':coleccionProcedencia', $coleccionProcedencia);
-            $stmt->bindParam(':altura', $altura);
-            $stmt->bindParam(':anchura', $anchura);
-            $stmt->bindParam(':profundidad', $profundidad);
-            $stmt->bindParam(':materialID', $materialID);
-            $stmt->bindParam(':tecnicaID', $tecnicaID);
-            $stmt->bindParam(':autorID', $autorID);
-            $stmt->bindParam(':titulo', $titulo);
-            $stmt->bindParam(':datacionID', $datacionID);
-            $stmt->bindParam(':ubicacionActualID', $ubicacionActualID);
-            $stmt->bindParam(':fechaRegistro', $fechaRegistro);
-            $stmt->bindParam(':numeroEjemplares', $numeroEjemplares);
-            $stmt->bindParam(':formaIngresoID', $formaIngresoID);
-            $stmt->bindParam(':fechaIngreso', $fechaIngreso);
-            $stmt->bindParam(':fuenteIngreso', $fuenteIngreso);
-            $stmt->bindParam(':bajaID', $bajaID);
-            $stmt->bindParam(':causaBajaID', $causaBajaID);
-            $stmt->bindParam(':fechaBaja', $fechaBaja);
-            $stmt->bindParam(':personaAutorizadaBaja', $personaAutorizadaBaja);
-            $stmt->bindParam(':estadoConservacionID', $estadoConservacionID);
-            $stmt->bindParam(':lugarEjecucion', $lugarEjecucion);
-            $stmt->bindParam(':lugarProcedencia', $lugarProcedencia);
-            $stmt->bindParam(':numeroTiraje', $numeroTiraje);
-            $stmt->bindParam(':otrosNrosIdentificacion', $otrosNrosIdentificacion);
-            $stmt->bindParam(':valoracionEconomica', $valoracionEconomica);
-            $stmt->bindParam(':bibliografia', $bibliografia);
-            $stmt->bindParam(':descripcion', $descripcion);
-            $stmt->bindParam(':historiaObjeto', $historiaObjeto);
-            $stmt->bindParam(':museoID', $museoID);
-            $stmt->bindParam(':activo', $activo);
-
-            $stmt->execute();
+            // Ejecutar la consulta pasando las variables en el mismo orden
+            $stmt->execute([
+                $registroN,
+                $usuarioID,
+                $imagen,
+                $nombre,
+                $clasificacionGenericaID,
+                $coleccionProcedencia,
+                $altura,
+                $anchura,
+                $profundidad,
+                $materialID,
+                $tecnicaID,
+                $autorID,
+                $titulo,
+                $datacionID,
+                $ubicacionActualID,
+                $fechaRegistro,
+                $numeroEjemplares,
+                $formaIngresoID,
+                $fechaIngreso,
+                $fuenteIngreso,
+                $bajaID,
+                $causaBajaID,
+                $fechaBaja,
+                $personaAutorizadaBaja,
+                $estadoConservacionID,
+                $lugarEjecucion,
+                $lugarProcedencia,
+                $numeroTiraje,
+                $otrosNrosIdentificacion,
+                $valoracionEconomica,
+                $bibliografia,
+                $descripcion,
+                $historiaObjeto,
+                $museoID,
+                $activo
+            ]);
         }
 
         function fitxesMostrar($id){
