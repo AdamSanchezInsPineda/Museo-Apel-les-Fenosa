36ff0a21ef71a697ca5aa844ade53734eef4d2c5
AdamSanchezInsPineda <adam.sanchez@inslapineda.cat>
2024-11-08 12:17:02 +0100

    new ubicaciones conceptualizado

--- public/resources/js/modules/ubicaciones/tree.js
+++ public/resources/js/modules/ubicaciones/tree.js
@@ -1,4 +1,6 @@
-function buildLocationTree(locations) {
+import {newUbicacion} from "/resources/js/modules/ubicaciones/new.js"
+
+export function buildLocationTree(locations) {
     const tree = [];
     const lookup = {};
 
@@ -21,45 +23,34 @@ function buildLocationTree(locations) {
         }
     });
 
-    console.log(tree);
-
     return tree;
 }
 
-function renderTree(node, container) {
+export function renderTree(node, container) {
     node.forEach(item => {
         const li = document.createElement('li');
+
+        const a = document.createElement('button');
+        a.textContent = "Añadir";
+        a.addEventListener("click", newUbicacion);
         
         if (item.children && item.children.length > 0) {
-            // Crear <details> y <summary> para los nodos con hijos
             const details = document.createElement('details');
             const summary = document.createElement('summary');
             summary.textContent = item.name;
             details.appendChild(summary);
+            summary.appendChild(a);
 
-            // Recursivamente renderizar los hijos dentro del <details>
             renderTree(item.children, details);
 
             li.appendChild(details);
         } else {
-            // Si no tiene hijos, simplemente renderizamos el nombre como <span>
             const span = document.createElement('span');
             span.textContent = item.name;
             li.appendChild(span);
+            li.appendChild(a);
         }
 
         container.appendChild(li);
     });
 }
-
-
-fetch('/ubicacions/json')
-    .then(response => response.json())
-    .then(locations => {
-        const tree = buildLocationTree(locations);
-        const container = document.getElementById('tree-container');
-        renderTree(tree, container);
-    });
-        function selectLocation(locationId) {
-            console.log('Ubicación seleccionada:', locationId);
-        }
