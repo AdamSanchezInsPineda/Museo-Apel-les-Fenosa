83f2ccd733a35524763b048bfbdc26cf169d7bd4
davidgarciafp <david.garcia.fernandez@inslapineda.cat>
2024-11-11 19:16:21 +0100

    pdf columnas v1

--- src/views/objects/informepdf.php
+++ src/views/objects/informepdf.php
@@ -6,105 +6,180 @@ use Spipu\Html2Pdf\Html2Pdf;
 $html2pdf = new Html2Pdf();
 $content = "
 <html>
-    <body class = 'verFichaCompleta'>
+    <head>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 20px;
+                line-height: 1.6;
+            }
+            table {
+                width: 100%; /* Asegura que la tabla ocupe todo el ancho */
+                border-collapse: collapse; /* Elimina el espacio entre celdas */
+            }
+            th, td {
+                padding: 10px; /* Espaciado interno de las celdas */
+                text-align: left; /* Alineación del texto */
+            }
+            h1, h2 {
+                color: #333;
+            }
+            .long-text {
+                grid-column: span 2; /* Esta clase hará que el elemento ocupe ambas columnas */
+            }
+        </style>
+    </head>
+    <body>
         <div>
-            <!--Contenido variable de la pagina-->
-            <h1>Fitxa completa de </h1>                        
-            <div>
-                <p>Nº de Registre: ".$cont[1][0]['RegistroNº']."</p> 
-                <div>
-                    <p>Nom: ".$cont[1][0]['Nombre']."</p>
-                    <p>Museu: ".$cont[1][0]['MuseoNombre']."</p>
-                    <p>Autor: ".$cont[1][0]['AutorNombre']."</p> 
-                    <p>Títol: ".$cont[1][0]['Titulo']."</p>
-                </div>
-            </div>
-            <div>
-                    
-            <div><h2>Ubicacions</h2><span></span></div>
-
-            <div>
-                <p>Data Inici Ubicació: ".$cont[1][0]['FechaInicioUbicacion']."</p>
-                <p>Data Final Ubicació: ".$cont[1][0]['FechaFinUbicacion']."</p>
-                <p>Comentari Ubicació: ".$cont[1][0]['ComentarioUbicacion']."</p>
-
-            </div>
-
-            <div><h2>Propietats</h2><span></span></div>
-
-            <div>
-                <p>Altura: ".$cont[1][0]['Altura']."</p>
-                <p>Datació: ".$cont[1][0]['DatacionDescripcion']."</p>
-                <p>Amplada: ".$cont[1][0]['Anchura']."</p>
-                <p>Any Inicial: ".$cont[1][0]['any_inicial']."</p>
-                <p>Profunditat: ".$cont[1][0]['Profundidad']."</p>
-                <p>Any Final: ".$cont[1][0]['any_final']."</p>
-                <p>Material: ".$cont[1][0]['MaterialNombre']."</p>
-                <p>Tècnica: ".$cont[1][0]['TecnicaNombre']."</p>
-            </div>
-
-            <div><h2>Baixa</h2><span></span></div>
-
-            <div>
-                <p>Baixa: ".$cont[1][0]['BajaValor']."</p>
-                <p>Causa Baixa: ".$cont[1][0]['CausaBajaValor']."</p>
-                <p>Data Baixa: ".$cont[1][0]['FechaBaja']."</p>
-                <p>Persona Autorizada Baja: ".$cont[1][0]['PersonaAutorizadaBaja']."</p>
-            </div>
-
-            <div><h2>Restauració</h2><span></span></div>
-
-            <div>
-                <p>Data Inici Restauració: ".$cont[1][0]['FechaInicioRestauracion']."</p>
-                <p>Data Final Restauració: ".$cont[1][0]['FechaFinRestauracion']."</p>
-                <p>Codi Restauració: ".$cont[1][0]['CodigoRestauracion']."</p>
-                <p>Nom Restaurador: ".$cont[1][0]['NombreRestaurador']."</p>
-                <p>Comentari Restauració: ".$cont[1][0]['ComentarioRestauracion']."</p>
-            </div>
-
-            <div><h2>Ingrés</h2><span></span></div>
-
-            <div>
-                <p>Forma d'Ingrés: ".$cont[1][0]['FormaIngresoValor']."</p>
-                <p>Font d'Ingrés: ".$cont[1][0]['FuenteIngreso']."</p>
-                <p>Data d'Ingrés: ".$cont[1][0]['FechaIngreso']."</p>
-                <p>Estat de conservació: ".$cont[1][0]['EstadoConservacionNombre']."</p>
-            </div>
+            <h1>Fitxa completa de ".$cont[1][0]['RegistroNº']."</h1>                        
+            <table>
+                <tr>
+                    <th>Nº de Registre</th>
+                    <td>".$cont[1][0]['RegistroNº']."</td>
+                </tr>
+                <tr>
+                    <th>Nom</th>
+                    <td>".$cont[1][0]['Nombre']."</td>
+                </tr>
+                <tr>
+                    <th>Museu</th>
+                    <td>".$cont[1][0]['MuseoNombre']."</td>
+                </tr>
+                <tr>
+                    <th>Autor</th>
+                    <td>".$cont[1][0]['AutorNombre']."</td>
+                </tr>
+                <tr>
+                    <th>Títol</th>
+                    <td>".$cont[1][0]['Titulo']."</td>
+                </tr>
+                <tr>
+                    <th>Ubicacions</th>
+                    <td>
+                        <p>Data Inici Ubicació: ".$cont[1][0]['FechaInicioUbicacion']."</p>
+                        <p>Data Final Ubicació: ".$cont[1][0]['FechaFinUbicacion']."</p>
+                        <p>Comentari Ubicació: ".$cont[1][0]['ComentarioUbicacion']."</p>
+                    </td>
+                </tr>
+                <tr>
+                    <tr>
+                        <th>Propietats</th>
+                    </tr>
+                    <tr>
+                        <td>Altura: ".$cont[1][0]['Altura']."</td>
+                        <td>Datació: ".$cont[1][0]['DatacionDescripcion']."</td>
+                    </tr>
+                    <tr>
+                        <td>Amplada: ".$cont[1][0]['Anchura']."</td>
+                        <td>Any Inicial: ".$cont[1][0]['any_inicial']."</td>
+                    </tr>
+                    <tr>
+                        <td>Profunditat: ".$cont[1][0]['Profundidad']."</td>
+                        <td>Any Final: ".$cont[1][0]['any_final']."</td>
+                    </tr>
+                    <tr>
+                        <td>Material: ".$cont[1][0]['MaterialNombre']."</td>
+                        <td>Tècnica: ".$cont[1][0]['TecnicaNombre']."</td>
+                    </tr>
+                </tr>
+                <tr>
+                    <tr>
+                        <th>Baixa</th>
+                    </tr>
+                    <tr>
+                        <td>Baixa: ".$cont[1][0]['BajaValor']."</td>
+                        <td>Causa Baixa: ".$cont[1][0]['CausaBajaValor']."</td>
+                    </tr>
+                    <tr>
+                        <td>Data Baixa: ".$cont[1][0]['FechaBaja']."</td>
+                        <td>Persona Autorizada Baja: ".$cont[1][0]['PersonaAutorizadaBaja']."</td>
+                    </tr>
+                </tr>
+                <tr>
+                    <tr>
+                        <th>Restauració</th>
+                    </tr>
+                    <tr>
+                        <td>Data Inici Restauració: ".$cont[1][0]['FechaInicioRestauracion']."</td>
+                        <td>Data Final Restauració: ".$cont[1][0]['FechaFinRestauracion']."</td>
+                    </tr>
+                    <tr>
+                        <td>Codi Restauració: ".$cont[1][0]['CodigoRestauracion']."</td>
+                        <td>Nom Restaurador: ".$cont[1][0]['NombreRestaurador']."</td>
+                    </tr>
+                    <tr>
+                        <td>Comentari Restauració: ".$cont[1][0]['ComentarioRestauracion']."</td>
+                    </tr>
+                </tr>
+                <tr>
+                    <tr>
+                        <th>Ingrés</th>
+                    </tr>
+                    <tr>  
+                        <td>Forma d'Ingrés: ".$cont[1][0]['FormaIngresoValor']."</td>
+                        <td>Font d'Ingrés: ".$cont[1][0]['FuenteIngreso']."</td>
+                    </tr>
+                    <tr>
+                        <td>Data d'Ingrés: ".$cont[1][0]['FechaIngreso']."</td>
+                        <td>Estat de conservació: ".$cont[1][0]['EstadoConservacionNombre']."</td>
+                    </tr>        
+                </tr>
+                <tr>
+                    <tr>
+                        <th>Exposicions</th>
+                    </tr>
+                    <tr>
+                        <td>Nom: ".$cont[1][0]['ExposicionNombre']."</td>
+                        <td>Lloc: ".$cont[1][0]['LugarExposicion']."</td>
+                    </tr>
+                    <tr>
+                        <td>Data Inicial: ".$cont[1][0]['FechaInicioExposicion']."</td>
+                        <td>Data Final: ".$cont[1][0]['FechaFinExposicion']."</td>
+                    </tr>
+                    <tr>
+                        <td>Tipus: ".$cont[1][0]['TipoExposicionNombre']."</td>
+                    </tr>        
+                </tr> 
                         
-            <div><h2>Exposicions</h2><span></span></div>
-
-            <div>
-                <p>Nom: ".$cont[1][0]['ExposicionNombre']."</p>
-                <p>Lloc: ".$cont[1][0]['LugarExposicion']."</p>
-                <p>Data Inicial: ".$cont[1][0]['FechaInicioExposicion']."</p>
-                <p>Data Final: ".$cont[1][0]['FechaFinExposicion']."</p>
-                <p>Tipus: ".$cont[1][0]['TipoExposicionNombre']."</p>
-            </div> 
-                        
-            <div><h2>Altres Dades</h2><span></span></div>
-
-            <div>
-                <p>Col·lecció de Procedencia: ".$cont[1][0]['ColeccionProcedencia']."</p>
-                <p>Lloc d'Execució: ".$cont[1][0]['LugarEjecucion']."</p>
-                <p>Nº de Tiratge: ".$cont[1][0]['NumeroTiraje']."</p>
-                <p>Valoració Económica: ".$cont[1][0]['ValoracionEconomica']."</p>
-                <p>Nº d'Exemplars: ".$cont[1][0]['NumeroEjemplares']."</p>
-                <p>Lloc de Procedencia: ".$cont[1][0]['LugarProcedencia']."</p>
-                <p>Altres Nº Identificació: ".$cont[1][0]['OtrosNrosIdentificacion']."</p>
-                <p>Classificació Genérica: ".$cont[1][0]['ClasificacionGenerica']."</p>
-                <p>Usuari que crea l'objecte: ".$cont[1][0]['UsuarioNombre']."</p>
-                <p>Data de Registre: ".$cont[1][0]['FechaRegistro']."</p>
-                <p>Bibliografia: ".$cont[1][0]['Bibliografia']."</p> 
-                <p>Descripció: ".$cont[1][0]['Descripcion']."</p>
-                <p>Història de l'objecte: ".$cont[1][0]['HistoriaObjeto']."</p>
-            </div>
-
-        </div>
-            
-            
+                <tr>
+                    <tr>
+                        <th>Altres Dades</th>
+                    </tr>
+                    <tr>
+                        <td>Col·lecció de Procedencia: ".$cont[1][0]['ColeccionProcedencia']."</td>
+                        <td>Lloc d'Execució: ".$cont[1][0]['LugarEjecucion']."</td>
+                    </tr>
+                    <tr>
+                        <td>Nº de Tiratge: ".$cont[1][0]['NumeroTiraje']."</td>
+                        <td>Valoració Económica: ".$cont[1][0]['ValoracionEconomica']."</td>
+                    </tr>
+                    <tr> 
+                        <td>Nº d'Exemplars: ".$cont[1][0]['NumeroEjemplares']."</td>
+                        <td>Lloc de Procedencia: ".$cont[1][0]['LugarProcedencia']."</td>
+                    </tr>
+                    <tr> 
+                        <td>Altres Nº Identificació: ".$cont[1][0]['OtrosNrosIdentificacion']."</td>
+                        <td>Classificació Genérica: ".$cont[1][0]['ClasificacionGenerica']."</td>
+                    </tr>
+                    <tr> 
+                        <td>Usuari que crea l'objecte: ".$cont[1][0]['UsuarioNombre']."</td>
+                        <td>Data de Registre: ".$cont[1][0]['FechaRegistro']."</td>
+                    </tr>
+                    <tr> 
+                        <td>Bibliografia: ".$cont[1][0]['Bibliografia']."</td> 
+                    </tr>
+                    <tr> 
+                        <td>Descripció: ".$cont[1][0]['Descripcion']."</td>
+                    </tr>
+                    <tr>    
+                        <td class='long-text'>Història de l'objecte: ".$cont[1][0]['HistoriaObjeto']."</td>
+                    </tr>
+                </tr>
+            </table>
         </div>
     </body>
-</html>";
+</html>
+";
 
 
 $html2pdf ->writeHTML($content);
