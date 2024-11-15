6fa1875a91b631271d03f9c86143bbf477534335
ArnauGiGi <arnau.gimenez@inslapineda.cat>
2024-11-15 13:53:10 +0100

    Comit search actualizado para ser generico

--- src/views/objects/objects.php
+++ src/views/objects/objects.php
@@ -6,12 +6,12 @@
     <div>
         <div>
             <div>
-                <input type="text" placeholder="Cercar">
+                <input type="text" placeholder="Cercar" id="search"> 
                 <a href="/registers/add">Fer un nou registre<img src="resources/images/plus.png" alt="Afegir registre"></a>
             </div>
             <table>
                 <?php
-                    $columns = ["Nº","Imatge","Objecte","Títol","Autor","Datació","Ubicació","Accions"];
+                    $columns = ["Nº","Imatge","Objecte","Títol","Autor","Ubicació","Datació","Accions"];
                     echo "<tr>";
 
                     foreach ($columns as $column) {
@@ -19,36 +19,15 @@
                     }
 
                     echo "</tr>";
-                    foreach ($registros as $registro) {
-                        echo "<tr>";
-                        foreach ($registro as $key => $dato) {
-                            if ($key == "Imagen") {
-                                echo "<td>";
-                                echo "<img src='resources/images/obras/{$dato}.jpg' alt='Foto de {$dato}' class='button1'>";
-                                echo "<div class='img-preview'>";
-                                echo "<button class='button2'>Salir</button>";
-                                echo "<img src='resources/images/obras/{$dato}.jpg' alt='Foto de {$dato}'>";
-                                echo "</div>";
-                                echo "</td>";
-                            } else {
-                                echo "<td>{$dato}</td>";
-                            }
-                        }
-
-                        echo "<td>";
-                        echo "<a href='/registers/{$registro['RegistroNº']}'><img src='resources/images/accions/eye.svg' alt='Ficha'></a>";
-                        if ($_SESSION['rol'] != "convidat") {
-                            echo "<a href='/registers/{$registro['RegistroNº']}/updateView'><img src='resources/images/accions/edit.svg' alt='Ficha'></a>";
-                            echo "<a href='/registers/{$registro['RegistroNº']}/delete'><img src='resources/images/accions/delete.png' alt='Ficha'></a>";
-                        }
-                        echo "</td>";
-                        echo "</tr>";
-                    }
                 ?>
+                <tbody class="tbody">
+
+                </tbody>
             </table>
         </div>
     </div>
     <!--Scripts-->
+    <script type="module" src="/resources/js/tableSearch/search.js"></script>
     <script src="resources/js/imagePreview.js"></script>
     <?php
     include "resources/components/footer.php";
