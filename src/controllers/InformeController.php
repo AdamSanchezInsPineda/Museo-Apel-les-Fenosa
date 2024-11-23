f99ae826c141815be08b17efdec6e7a3f17814ad
ArnauGiGi <arnau.gimenez@inslapineda.cat>
2024-11-23 18:05:13 +0100

    busquedaactualizacion

--- src/controllers/InformeController.php
+++ src/controllers/InformeController.php
@@ -1,108 +1,44 @@
 <?php
 
-    class InformeController{
+    class InformeController extends Controller{
+
+        protected $exposicio;
+        protected $objeto;
+
+        public function __construct()
+        {
+            parent::__construct();
+            $this->exposicio = new Exposicio();
+            $this->objeto = new Objeto();
+        }
 
         public function index($registroN){
-            session_start();
-            if (!isset($_SESSION['nom'])){
-                $state = false;
-            }
-            else{
-                $user = new Usuario();
-                $state = $user->comprovarUsuario($_SESSION['nom'] , $_SESSION['password']);
-            }
-            if($state){
-                $objeto = new Objeto();
-                $this->render('objects/informes/informepdf', ['cont' => [$registroN, $objeto->fitxesMostrar($registroN)]]);
-                exit;
+            $this->checkRole(['admin', 'tecnic', 'convidat']);
+                
+            $this->render('objects/informes/informepdf', ['cont' => [$registroN, $this->objeto->fitxesMostrar($registroN)]]);
+            exit;
 
-            } else {
-                session_unset();
-                session_destroy();
-                $error = " La sessió no s'ha iniciat ";
-                $this->render("login", ["error"=> $error]);
-                exit;
-            }
         }
 
         public function basic($registroN){
-            session_start();
-            if (!isset($_SESSION['nom'])){
-                $state = false;
-            }
-            else{
-                $user = new Usuario();
-                $state = $user->comprovarUsuario($_SESSION['nom'] , $_SESSION['password']);
-            }
-            if($state){
-                $objeto = new Objeto();
-                $this->render('objects/informes/informeBasicapdf', ['cont' => [$registroN, $objeto->fitxesMostrar($registroN)]]);
-                exit;
-
-            } else {
-                session_unset();
-                session_destroy();
-                $error = " La sessió no s'ha iniciat ";
-                $this->render("login", ["error"=> $error]);
-                exit;
-            }
+            $this->checkRole(['admin', 'tecnic', 'convidat']);
+                
+            $this->render('objects/informes/informeBasicapdf', ['cont' => [$registroN, $this->objeto->fitxesMostrar($registroN)]]);
+            exit;         
         }
 
         public function llibre(){
-            session_start();
-            if (!isset($_SESSION['nom'])){
-                $state = false;
-            }
-            else{
-                $user = new Usuario();
-                $state = $user->comprovarUsuario($_SESSION['nom'] , $_SESSION['password']);
-            }
-            if($state){
-                $objeto = new Objeto();
-                $this->render('objects/informes/llibreRegistre', ['cont' => [$objeto->generarLlibre()]]);
-                exit;
+            $this->checkRole(['admin', 'tecnic', 'convidat']);
+            
+            $this->render('objects/informes/llibreRegistre', ['cont' => [$this->objeto->generarLlibre()]]);
+            exit;
 
-            } else {
-                session_unset();
-                session_destroy();
-                $error = " La sessió no s'ha iniciat ";
-                $this->render("login", ["error"=> $error]);
-                exit;
-            }
         }
         public function prestec($registroN){
-            session_start();
-            if (!isset($_SESSION['nom'])){
-                $state = false;
-            }
-            else{
-                $user = new Usuario();
-                $state = $user->comprovarUsuario($_SESSION['nom'] , $_SESSION['password']);
-            }
-            if($state){
-                $objeto = new Objeto();
-                $this->render('objects/informes/formulariPrestec', ['cont' => [$registroN, $objeto->fitxesMostrar($registroN)]]);
-                exit;
+            $this->checkRole(['admin', 'tecnic', 'convidat']);
+            
+            $this->render('objects/informes/formulariPrestec', ['cont' => [$registroN, $this->objeto->fitxesMostrar($registroN)]]);
+            exit;
 
-            } else {
-                session_unset();
-                session_destroy();
-                $error = " La sessió no s'ha iniciat ";
-                $this->render("login", ["error"=> $error]);
-                exit;
-            }
         }
-
-        private function render($view, $data = []) {
-            extract($data);
-            $viewFile = "../src/views/$view.php";
-            if (file_exists($viewFile)) {
-                require $viewFile;
-            } else {
-                echo "View $view not found.";
-            }
-        }
-
     }
-
-?>
