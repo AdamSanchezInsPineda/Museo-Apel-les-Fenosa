333219c94069eb9d50bb3db5013f1a7060c639c0
davidgarciafp <david.garcia.fernandez@inslapineda.cat>
2024-12-11 12:40:50 +0100

    validate inicio

--- public/resources/js/validateForm.js
+++ public/resources/js/validateForm.js
@@ -1,6 +1,17 @@
 function validateForm() {
     const checkboxes = document.querySelectorAll('input[name="afegir[]"]:checked');
+    const registroInput = document.getElementById('RegistroNº');
     const errorMessage = document.getElementById('errorMessage');
+    const registroValue = registroInput.value.trim();
+    
+    // Expresión regular para validar el formato del número de registro
+    const regex = /^[A-Za-z]?\d{5}$/; // Opcionalmente una letra seguida de 5 dígitos
+
+    if (!regex.test(registroValue)) {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = 'El número de registro debe ser 5 dígitos y puede tener una letra al principio.';
+        return false;
+    }
     
     if (checkboxes.length === 0) {
         errorMessage.style.display = 'block';
--- src/views/objects/createObject.php
+++ src/views/objects/createObject.php
@@ -5,7 +5,7 @@
     <!--Contenido variable de la pagina-->
     <main>
         <!--Contenido variable de la pagina-->              
-        <form action="/registers/update" method="POST" enctype="multipart/form-data">
+        <form action="/registers/create" method="POST" enctype="multipart/form-data">
         <h1>Afegir un registre</h1>
             <div>
                 <div>
@@ -20,9 +20,9 @@
                     <label for="nombre">Nom:
                     <input type="text" id="nombre" name="nombre" required></label>
                     <label for="museo">Museu:
-                    <input type="text" id="museo" name="museo" required></label>
+                    <input type="text" id="museo" name="museo"></label>
                     <label for="autor">Autor:
-                    <input type="text" id="autor" name="autor" required></label>
+                    <input type="text" id="autor" name="autor"></label>
                     <label for="titulo">Títol:
                     <input type="text" id="titulo" name="titulo" required></label>
                 </div>
@@ -34,11 +34,11 @@
 
                 <section class="hide">
                     <label for="fechaInicioUbicacion">Data Inici Ubicació:
-                    <input type="datetime-local" id="fechaInicioUbicacion" name="fechaInicioUbicacion" required></label>
+                    <input type="datetime-local" id="fechaInicioUbicacion" name="fechaInicioUbicacion"></label>
                     <label for="fechaFinUbicacion">Data Final Ubicació:
-                    <input type="datetime-local" id="fechaFinUbicacion" name="fechaFinUbicacion" required></label>
+                    <input type="datetime-local" id="fechaFinUbicacion" name="fechaFinUbicacion"></label>
                     <label for="comentarioUbicacion">Comentari Ubicació:
-                    <textarea id="comentarioUbicacion" name="comentarioUbicacion" required></textarea></label>
+                    <textarea id="comentarioUbicacion" name="comentarioUbicacion"></textarea></label>
 
                 </section>
 
@@ -46,111 +46,107 @@
 
                 <section class="hide">
                     <label for="altura">Altura:
-                    <input type="text" id="altura" name="altura" required></label>
+                    <input type="text" id="altura" name="altura"></label>
                     <label for="datacion">Datació:
-                    <input type="text" id="datacion" name="datacion" required></label>
+                    <input type="text" id="datacion" name="datacion"></label>
                     <label for="anchura">Amplada:
-                    <input type="text" id="anchura" name="anchura" required></label>
+                    <input type="text" id="anchura" name="anchura"></label>
                     <label for="anyInicial">Any Inicial:
-                    <input type="text" id="anyInicial" name="anyInicial" required></label>
+                    <input type="text" id="anyInicial" name="anyInicial"></label>
                     <label for="profundidad">Profunditat:
-                    <input type="text" id="profundidad" name="profundidad" required></label>
+                    <input type="text" id="profundidad" name="profundidad"></label>
                     <label for="anyFinal">Any Final:
-                    <input type="text" id="anyFinal" name="anyFinal" required></label>
+                    <input type="text" id="anyFinal" name="anyFinal"></label>
                     <label for="material">Material:
-                    <input type="text" id="material" name="material" required></label>
+                    <input type="text" id="material" name="material"></label>
                     <label for="tecnica">Tècnica:
-                    <input type="text" id="tecnica" name="tecnica" required></label>
+                    <input type="text" id="tecnica" name="tecnica"></label>
                 </section>
 
                 <div class="toggle-hide"><h2>Baixa</h2><span></span></div>
 
                 <section class="hide">
                     <label for="baja">Baixa:
-                    <input type="text" id="baja" name="baja" required></label>
+                    <input type="text" id="baja" name="baja"></label>
                     <label for="causaBaja">Causa Baixa:
-                    <input type="text" id="causaBaja" name="causaBaja" required></label>
+                    <input type="text" id="causaBaja" name="causaBaja"></label>
                     <label for="fechaBaja">Data Baixa:
-                    <input type="datetime-local" id="fechaBaja" name="fechaBaja" required></label>
+                    <input type="datetime-local" id="fechaBaja" name="fechaBaja"></label>
                     <label for="personaAutorizadaBaja">Persona Autorizada Baja:
-                    <input type="text" id="personaAutorizadaBaja" name="personaAutorizadaBaja" required></label>
+                    <input type="text" id="personaAutorizadaBaja" name="personaAutorizadaBaja"></label>
                 </section>
 
                 <div class="toggle-hide"><h2>Restauració</h2><span></span></div>
 
                 <section class="hide">
                     <label for="fechaInicioRestauracion">Data Inici Restauració:
-                    <input type="datetime-local" id="fechaInicioRestauracion" name="fechaInicioRestauracion" required></label>
+                    <input type="datetime-local" id="fechaInicioRestauracion" name="fechaInicioRestauracion"></label>
                     <label for="fechaFinRestauracion">Data Final Restauració:
-                    <input type="datetime-local" id="fechaFinRestauracion" name="fechaFinRestauracion" required></label>
+                    <input type="datetime-local" id="fechaFinRestauracion" name="fechaFinRestauracion"></label>
                     <label for="codigoRestauracion">Codi Restauració:
-                    <input type="text" id=" codigoRestauracion" name="codigoRestauracion" required></label>
+                    <input type="text" id=" codigoRestauracion" name="codigoRestauracion"></label>
                     <label for="nombreRestaurador">Nom Restaurador:
-                    <input type="text" id="nombreRestaurador" name="nombreRestaurador" required></label>
+                    <input type="text" id="nombreRestaurador" name="nombreRestaurador"></label>
                     <label for="comentarioRestauracion">Comentari Restauració:
-                    <textarea id="comentarioRestauracion" name="comentarioRestauracion" required></textarea></label>
+                    <textarea id="comentarioRestauracion" name="comentarioRestauracion"></textarea></label>
                 </section>
 
                 <div class="toggle-hide"><h2>Ingrés</h2><span></span></div>
 
                 <section class="hide">
                     <label for="formaIngreso">Forma d'Ingrés:
-                    <input type="text" id="formaIngreso" name="formaIngreso" required></label>
+                    <input type="text" id="formaIngreso" name="formaIngreso"></label>
                     <label for="fuenteIngreso">Font d'Ingrés:
-                    <input type="text" id="fuenteIngreso" name="fuenteIngreso" required></label>
+                    <input type="text" id="fuenteIngreso" name="fuenteIngreso"></label>
                     <label for="fechaIngreso">Data d'Ingrés:
-                    <input type="datetime-local" id="fechaIngreso" name="fechaIngreso" required></label>
+                    <input type="datetime-local" id="fechaIngreso" name="fechaIngreso"></label>
                     <label for="estadoConservacion">Estat de conservació:
-                    <input type="text" id="estadoConservacion" name="estadoConservacion" required></label>
+                    <input type="text" id="estadoConservacion" name="estadoConservacion"></label>
                 </section>
 
                 <div class="toggle-hide"><h2>Exposicions</h2><span></span></div>
 
                 <section class="hide">
                     <label for="exposicionNombre">Nom:
-                    <input type="text" id="exposicionNombre" name="exposicionNombre" required></label>
+                    <input type="text" id="exposicionNombre" name="exposicionNombre"></label>
                     <label for="lugarExposicion">Lloc:
-                    <input type="text" id="lugarExposicion" name="lugarExposicion" required></label>
+                    <input type="text" id="lugarExposicion" name="lugarExposicion"></label>
                     <label for="fechaInicioExposicion">Data Inicial:
-                    <input type="datetime-local" id="fechaInicioExposicion" name="fechaInicioExposicion" required></label>
+                    <input type="datetime-local" id="fechaInicioExposicion" name="fechaInicioExposicion"></label>
                     <label for="fechaFinExposicion">Data Final:
-                    <input type="datetime-local" id="fechaFinExposicion" name="fechaFinExposicion" required></label>
+                    <input type="datetime-local" id="fechaFinExposicion" name="fechaFinExposicion"></label>
                     <label for="tipoExposicion">Tipus:
-                    <input type="text" id="tipoExposicion" name="tipoExposicion" required></label>
+                    <input type="text" id="tipoExposicion" name="tipoExposicion"></label>
                 </section>
 
                 <div class="toggle-hide"><h2>Altres Dades</h2><span></span></div>
 
                 <section class="hide">
                     <label for="coleccionProcedencia">Col·lecció de Procedencia:
-                    <input type="text" id="coleccionProcedencia" name="coleccionProcedencia" required></label>
+                    <input type="text" id="coleccionProcedencia" name="coleccionProcedencia"></label>
                     <label for="lugarEjecucion">Lloc d'Execució:
-                    <input type="text" id="lugarEjecucion" name="lugarEjecucion" required></label>
+                    <input type="text" id="lugarEjecucion" name="lugarEjecucion"></label>
                     <label for="numeroTiraje">Nº de Tiratge:
-                    <input type="text" id="numeroTiraje" name="numeroTiraje" required></label>
+                    <input type="text" id="numeroTiraje" name="numeroTiraje"></label>
                     <label for="valoracionEconomica">Valoració Económica:
-                    <input type="text" id="valoracionEconomica" name="valoracionEconomica" required></label>
+                    <input type="text" id="valoracionEconomica" name="valoracionEconomica"></label>
                     <label for="numeroEjemplares">Nº d'Exemplars:
-                    <input type="text" id="numeroEjemplares" name="numeroEjemplares" required></label>
+                    <input type="text" id="numeroEjemplares" name="numeroEjemplares"></label>
                     <label for="lugarProcedencia">Lloc de Procedencia:
-                    <input type="text" id="lugarProcedencia" name="lugarProcedencia" required></label>
+                    <input type="text" id="lugarProcedencia" name="lugarProcedencia"></label>
                     <label for="otrosNrosIdentificacion">Altres Nº Identificació:
-                    <input type="text" id="otrosNrosIdentificacion" name="otrosNrosIdentificacion" required></label>
+                    <input type="text" id="otrosNrosIdentificacion" name="otrosNrosIdentificacion"></label>
                     <label for="clasificacionGenerica">Classificació Genérica:
-                    <input type="text" id="clasificacionGenerica" name="clasificacionGenerica" required></label>
-                    <label for="usuarioNombre">Usuari que crea l'objecte:
-                    <input type="text" id="usuarioNombre" name="usuarioNombre" required readonly></label>
-                    <label for="fechaRegistro">Data de Registre:
-                    <input type="datetime-local" id="fechaRegistro" name="fechaRegistro" required readonly></label>
+                    <input type="text" id="clasificacionGenerica" name="clasificacionGenerica"></label>
                     <label for="bibliografia">Bibliografia:
-                    <textarea id="bibliografia" name="bibliografia" required></textarea></label>
+                    <textarea id="bibliografia" name="bibliografia"></textarea></label>
                     <label for="descripcion">Descripció:
-                    <textarea id="descripcion" name="descripcion" required></textarea></label>
+                    <textarea id="descripcion" name="descripcion"></textarea></label>
                     <label for="historiaObjeto">Història de l'objecte:
-                    <textarea id="historiaObjeto" name="historiaObjeto" required></textarea></label>
+                    <textarea id="historiaObjeto" name="historiaObjeto"></textarea></label>
                 </section>
             </div>
-            <button type="submit">Actualizar</button>
+            <button type="submit">Crear</button>
         </form>
     </main>
     
@@ -169,6 +165,7 @@
             });
         });
     </script>
+    <script src="/resources/js/validateForm.js"></script>
     <?php
     include "resources/components/footer.php";
     ?>
